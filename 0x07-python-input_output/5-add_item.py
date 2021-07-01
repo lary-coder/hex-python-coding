c03fd5a5cd751a3f6f82a7b0e7ac21b044620257
Lary-Coder <laryssarib@gmail.com>
2021-07-01 15:11:49 -0300

    5-add_item

--- 0x07-python-input_output/5-add_item.py
+++ 0x07-python-input_output/5-add_item.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-from os import path
+import json
 from sys import argv
-save_to_json_file = __import__('3-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('4-load_from_json_file').load_from_json_file
+"""
+This file contains a function that adds
+all arguments to a python list and saves
+to a file
+"""
 
-if path.exists('add_item.json'):
-    obj_json_file = load_from_json_file('add_item.json')
-else:
-    obj_json_file = []
 
-for i in range(1, len(argv)):
-    obj_json_file.append(argv[i])
+save_json = __import__('3-save_to_json_file').save_to_json_file
+load_json = __import__('4-load_from_json_file').load_from_json_file
 
-save_to_json_file(obj_json_file, 'add_item.json')
+filename = 'add_item.json'
+
+try:
+    new = load_json(filename)
+except (ValueError, FileNotFoundError):
+    new = []
+
+new = new + argv[1:]
+save_json(new, filename)
