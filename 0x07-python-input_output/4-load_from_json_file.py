2087109f6618497ed940be274a9f1e587c8d2c0d
Lary-Coder <laryssarib@gmail.com>
2021-07-01 15:27:36 -0300

    4-load_from_json_file

--- 0x07-python-input_output/4-load_from_json_file.py
+++ 0x07-python-input_output/4-load_from_json_file.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""
-Contains the "load_from_json_file" function
-"""
+"""This module creates an Object from a JSON file"""
+
 
 import json
 
 
 def load_from_json_file(filename):
-    """creates an Object from a "JSON file" """
-    with open(filename, 'r', encoding='utf-8') as f:
-        return json.load(f)
+    """creates an Object from a JSON file
+    Args:
+    filename (str): JSON file to create an Object from
+    Returns:
+    object: JSON object
+    """
+    with open(filename, encoding='utf-8') as file:
+        return json.loads(file.read())
