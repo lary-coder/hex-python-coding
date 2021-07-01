3b8ecba5f04b7ca6f1884acfb47c33d12be765e7
Lary-Coder <laryssarib@gmail.com>
2021-07-01 15:32:49 -0300

    5-add_item

--- 0x07-python-input_output/5-add_item.py
+++ 0x07-python-input_output/5-add_item.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-import json
+"""This module creates a script that adds args to Python list and saves them"""
+
+from os import path
 from sys import argv
-"""
-This file contains a function that adds
-all arguments to a python list and saves
-to a file
-"""
+save_to_json_file = __import__('3-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('4-load_from_json_file').load_from_json_file
 
-
-save_json = __import__('3-save_to_json_file').save_to_json_file
-load_json = __import__('4-load_from_json_file').load_from_json_file
-
-filename = 'add_item.json'
-
-try:
-    new = load_json(filename)
-except (ValueError, FileNotFoundError):
-    new = []
-
-new = new + argv[1:]
-save_json(new, filename)
+if path.exists("add_item.json") is False:
+    save_to_json_file([], "add_item.json")
+my_list = load_from_json_file("add_item.json")
+for i in range(1, len(argv)):
+    my_list.append(argv[i])
+save_to_json_file(my_list, "add_item.json")
