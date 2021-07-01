2cf0fc5f1ca64e70a9d2093a54fa315abd74d8d1
Lary-Coder <laryssarib@gmail.com>
2021-07-01 14:19:18 -0300

    5-add_item

--- 0x07-python-input_output/5-add_item.py
+++ 0x07-python-input_output/5-add_item.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """
-script to save and load
+Script that adds all arguments to a Python list, and then saves them to a file
 """
-import sys
 
-save_json = __import__('3-save_to_json_file').save_to_json_file
-load_json = __import__('4-load_from_json_file').load_from_json_file
+from sys import argv
+save_to_json_file = __import__("3-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("4-load_from_json_file").load_from_json_file
 
-file = "add_item.json"
+filename = "add_item.json"
+
 try:
-    new = load_json(file)
-except (ValueError, FileNotFoundError):
-    new = []
-for args in sys.argv[1:]:
-    new.append(args)
-save_json(new, file)
+    json_list = load_from_json_file(filename)
+except:
+    json_list = []
+
+for arg in argv[1:]:
+    json_list.append(arg)
+
+save_to_json_file(json_list, filename)
