e4633d453eb6d017ba748e971f740b1cbcbf1b44
Lary-Coder <laryssarib@gmail.com>
2021-07-01 14:52:01 -0300

    4-load_from_json_file

--- 0x07-python-input_output/4-load_from_json_file.py
+++ 0x07-python-input_output/4-load_from_json_file.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# 4-load_from_json_file.py
-# Laryssa Ribeiro
-"""Defines a JSON file-reading function."""
+"""
+Contains the "load_from_json_file" function
+"""
+
 import json
 
 
 def load_from_json_file(filename):
-    """Create a Python object from a JSON file."""
-    with open(filename) as f:
+    """creates an Object from a "JSON file" """
+    with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
