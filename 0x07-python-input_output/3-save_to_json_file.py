f5f8c0624f1819b6961ba26838403c72c75aed67
Lary-Coder <laryssarib@gmail.com>
2021-07-05 14:56:42 -0300

    3-save_to_json_file

--- 0x07-python-input_output/3-save_to_json_file.py
+++ 0x07-python-input_output/3-save_to_json_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Define the writing json strings to a text file"""
-
-
+""" Module """
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Writes an Object to a text file, using a JSON representation"""
-    with open(filename, 'w', encoding='utf-8') as myFile:
-        return myFile.write(json.dumps(my_obj))
+    """ Save Object to a file """
+    with open(filename, 'w') as f:
+        str_json = json.dumps(my_obj)
+        f.write(str_json)
+    f.close()
